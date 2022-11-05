d18290ea87d418f2305070e3f8b36895be621165
Ersapta Aristo <aristoersapta@gmail.com>
2022-11-06 00:48:59 +0700

    [master] refactor images gallery

---
+++ utils/data/index.js
@@ -0,0 +1,174 @@
+const data = {
+    imagesGallery: [
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/1a%20resize.png',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F1a.png',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/1b%20resize.png',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F1b.png',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/1c%20resize.png',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F1c.png',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/2a%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F2a.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/2b%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F2b.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/2c%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F2c.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/3a%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F3a.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/4b%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F4b.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/5a%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F5a.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/5b%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F5b.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/5c%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F5c.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/6a%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F6a.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/6c%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F6c.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/7a%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F7a.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/7b%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F7b.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        }, 
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/8a%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F8a.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/8b%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F8b.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/8c%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F8c.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/9a%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F9a.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/9b%20reszie.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F9b.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/10a%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F10a.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/10b%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F10b.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/10c%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F10c.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/11a%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F11a.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/11b%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F11b.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/11c%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F11c.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/11d%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F11d.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+        {
+          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/DSC_8401%20resize.jpg',
+          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2FDSC_8401.jpg',
+          thumbnailHeight: '50px',
+          thumbnailWidth: '50px'
+        },
+    ]
+};
+
+export default data;
