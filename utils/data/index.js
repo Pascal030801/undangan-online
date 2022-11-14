1c092aa7b953a00ca5f1f68e317098071d9594c3
aristoersapta@gmail.com <aristoersapta@gmail.com>
2022-11-15 05:27:54 +0700

    [master] delete some photo and add new invitation

--- utils/data/index.js
+++ utils/data/index.js
@@ -1,12 +1,6 @@
 const data = {
     imagesGallery: [
         {
-          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/1a%20resize.png',
-          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F1a.png',
-          thumbnailHeight: '50px',
-          thumbnailWidth: '50px'
-        },
-        {
           original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/1b%20resize.png',
           thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F1b.png',
           thumbnailHeight: '50px',
@@ -15,12 +9,6 @@ const data = {
         {
           original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/1c%20resize.png',
           thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F1c.png',
-          thumbnailHeight: '50px',
-          thumbnailWidth: '50px'
-        },
-        {
-          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/2a%20resize.jpg',
-          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F2a.jpg',
           thumbnailHeight: '50px',
           thumbnailWidth: '50px'
         },
@@ -43,20 +31,8 @@ const data = {
           thumbnailWidth: '50px'
         },
         {
-          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/4b%20resize.jpg',
-          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F4b.jpg',
-          thumbnailHeight: '50px',
-          thumbnailWidth: '50px'
-        },
-        {
           original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/5b%20resize.jpg',
           thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F5b.jpg',
-          thumbnailHeight: '50px',
-          thumbnailWidth: '50px'
-        },
-        {
-          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/5c%20resize.jpg',
-          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F5c.jpg',
           thumbnailHeight: '50px',
           thumbnailWidth: '50px'
         },
@@ -73,12 +49,6 @@ const data = {
           thumbnailWidth: '50px'
         },
         {
-          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/DSC_8401%20resize.jpg',
-          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2FDSC_8401.jpg',
-          thumbnailHeight: '50px',
-          thumbnailWidth: '50px'
-        },
-        {
           original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/7b%20resize.jpg',
           thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F7b.jpg',
           thumbnailHeight: '50px',
@@ -87,12 +57,6 @@ const data = {
         {
           original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/8a%20resize.jpg',
           thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F8a.jpg',
-          thumbnailHeight: '50px',
-          thumbnailWidth: '50px'
-        },
-        {
-          original: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/8b%20resize.jpg',
-          thumbnail: 'https://sin1.contabostorage.com/de4425191d2e47d69db71db4a9e57219:undanganyudaayu/thumbnail%2F8b.jpg',
           thumbnailHeight: '50px',
           thumbnailWidth: '50px'
         },
